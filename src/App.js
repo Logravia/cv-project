33e12154ef341947e02345b8fd383407e0506f65
Jānis Alunāns <j.alunans@gmail.com>
2022-12-29 21:08:40 +0200

    Add state modifying methods

--- src/App.js
+++ src/App.js
@@ -2,7 +2,41 @@ import './App.css';
 import { Component } from 'react';
 
 class App extends Component {
-  render () {
+  constructor(props) {
+    super(props)
+    this.state = {
+      name: "John Doe",
+      image: "",
+      short: "I am a hardworking work-a-holic",
+      contactInfo: [{address: "Helmdeep"}, {phone: 123},
+                    {email: "email@mail.com"}, {online: "Linkedin: ABC"}],
+      education: [{institution: "Harvard", enrollment:"2019",
+                   graduation: "2020", title: "MA in BA"}],
+      work: [{workplace: "Starbucks", responsibilites: "Coffee Making"}],
+      skills: [{name: "Programming", description: "Really proficient"}]
+    }
+
+    this.addTo = this.addTo.bind(this);
+    this.removeFrom = this.removeFrom.bind(this);
+    this.update = this.update.bind(this);
+  }
+
+  addTo(prop, value) {
+    let list = this.state[prop];
+    this.setState({[prop]: list.concat(value)})
+  }
+
+  removeFrom(prop, thisIndex){
+    let list = this.state[prop];
+    list.splice(thisIndex, 1);
+    this.setState({[prop]: list});
+  }
+
+  update(prop, value) {
+    this.setState({[prop]: value});
+  }
+
+  render() {
     return (
       <div className="App">
         <h1>Hello world</h1>
